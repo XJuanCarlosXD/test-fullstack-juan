7befca9abb4fd32df281062c0f346d47dc295210
XJuanCarlosXD <juancarlos192003@gmail.com>
2024-02-20 17:19:45 -0400

    update 19 files

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  basePath: string = 'assets/images/';
+  itemsAse: { image: string; link: string }[] = [];
 
-  constructor() {}
-
+  constructor() {
+    this.itemsAse = [
+      { image: 'logo-la_colonial.png', link: '#' },
+      { image: 'futuro_logo.svg', link: '#' },
+      { image: 'patria_logo.png', link: '#' },
+      { image: 'logo_humano.svg', link: '#' },
+      { image: 'logo_aps.png', link: '#' },
+      { image: 'logo_atlantica.png', link: '#' },
+    ];
+  }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,10 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  menuItems: { text: string; link: string; icon: string; tab: string }[] = [];
 
-  constructor() {}
-
+  constructor() {
+    this.menuItems = [
+      { text: 'Inicio', link: '/tabs/tab1', icon: 'home-outline', tab: 'tab1' },
+      {
+        text: 'Guardados',
+        link: '/tabs/tab2',
+        icon: 'bag-outline',
+        tab: 'tab2',
+      },
+      {
+        text: 'Mi poliza',
+        link: '/tabs/tab3',
+        icon: 'clipboard-outline',
+        tab: 'tab3',
+      },
+      {
+        text: 'Ayuda',
+        link: '/tabs/tab4',
+        icon: 'chatbox-ellipses-outline',
+        tab: 'tab4',
+      },
+    ];
+  }
 }
